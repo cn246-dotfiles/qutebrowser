6aa0dcf13ec0cbb74232879aca654c1fee0ecccb
Chuck Nemeth <chuckn246@gmail.com>
2020-03-21 11:16:32 -0400

    Initial Commit

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,23 @@
+# COLORS
+main_color = '#2f343f'
+
+config.bind('<Ctrl-Shift-m>', 'hint links spawn --detach mpv {hint-url}')
+
+c.colors.completion.category.bg = main_color
+c.colors.completion.even.bg = '#434a59'
+c.colors.completion.odd.bg = '#393f4d'
+c.colors.completion.scrollbar.bg = '#2f343f'
+c.colors.downloads.bar.bg = '#2f343f'
+c.colors.messages.error.bg = '#7a2021'
+c.colors.statusbar.command.private.bg = '#5b6b8c'
+c.colors.statusbar.normal.bg = '#ffce61'
+c.colors.statusbar.private.bg = '#2f343f'
+c.colors.statusbar.url.success.https.fg = 'lime'
+c.colors.tabs.even.bg = '#393f4d'
+c.colors.tabs.odd.bg = '#303540'
+c.colors.tabs.selected.even.bg = '#69748c'
+c.colors.tabs.selected.odd.bg = '#69748c'
+
+c.content.private_browsing = True
+
+c.editor.command = ['urxvt', '-e', 'vim', '{}']
