a22e4007594ee5e6478f682b1d8760420646cd8f
Chuck Nemeth <chuckn246@gmail.com>
2020-04-08 19:44:13 -0400

    Color variables, undo private browsing

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,18 +6,18 @@ config.bind('<Ctrl-Shift-m>', 'hint links spawn --detach mpv {hint-url}')
 c.colors.completion.category.bg = main_color
 c.colors.completion.even.bg = '#434a59'
 c.colors.completion.odd.bg = '#393f4d'
-c.colors.completion.scrollbar.bg = '#2f343f'
-c.colors.downloads.bar.bg = '#2f343f'
+c.colors.completion.scrollbar.bg = main_color
+c.colors.downloads.bar.bg = main_color
 c.colors.messages.error.bg = '#7a2021'
 c.colors.statusbar.command.private.bg = '#5b6b8c'
-c.colors.statusbar.normal.bg = '#ffce61'
-c.colors.statusbar.private.bg = '#2f343f'
+c.colors.statusbar.normal.bg = main_color
+c.colors.statusbar.private.bg = '#333333'
 c.colors.statusbar.url.success.https.fg = 'lime'
 c.colors.tabs.even.bg = '#393f4d'
 c.colors.tabs.odd.bg = '#303540'
 c.colors.tabs.selected.even.bg = '#69748c'
 c.colors.tabs.selected.odd.bg = '#69748c'
 
-c.content.private_browsing = True
+c.content.private_browsing = False
 
 c.editor.command = ['urxvt', '-e', 'vim', '{}']
