c136182ad066a0666c3073ce1a2bbc50a873d1e7
Chuck Nemeth <chuckn246@gmail.com>
2020-08-13 08:53:13 -0400

    Remove old colors, format dracula code, full path for mpv command

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,7 @@
 # Commands
 c.content.private_browsing = False
 c.editor.command = ['urxvt', '-e', 'vim', '{}']
-config.bind('<Ctrl-Shift-m>', 'hint links spawn --detach mpv --force-window=immediate {hint-url}')
+config.bind('<Ctrl-Shift-m>', 'hint links spawn --detach /usr/bin/mpv --force-window=immediate {hint-url}')
 
 # COLORS
 import dracula.draw
@@ -9,27 +9,34 @@ import dracula.draw
 # Load existing settings made via :set
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
-        'spacing': {
-                    'vertical': 6,
-                            'horizontal': 8
-                                }
-        })
-
-#main_color = '#2f343f'
-#c.colors.completion.category.bg = main_color
-#c.colors.completion.even.bg = '#434a59'
-#c.colors.completion.odd.bg = '#393f4d'
-#c.colors.completion.scrollbar.bg = main_color
-#c.colors.downloads.bar.bg = main_color
-#c.colors.messages.error.bg = '#7a2021'
-#c.colors.statusbar.command.private.bg = '#5b6b8c'
-#c.colors.statusbar.normal.bg = main_color
-#c.colors.statusbar.private.bg = '#333333'
-#c.colors.statusbar.url.success.https.fg = 'lime'
-#c.colors.tabs.even.bg = '#393f4d'
-#c.colors.tabs.odd.bg = '#303540'
-##c.colors.tabs.selected.even.bg = '#69748c'
-##c.colors.tabs.selected.odd.bg = '#69748c'
 #
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+# Set search engines
+c.url.searchengines = {
+    'DEFAULT':  'https://google.com/search?hl=en&q={}',
+    '!a':       'https://www.amazon.com/s?k={}',
+    '!d':       'https://duckduckgo.com/?ia=web&q={}',
+    '!dd':      'https://thefreedictionary.com/{}',
+    '!e':       'https://www.ebay.com/sch/i.html?_nkw={}',
+    '!fb':      'https://www.facebook.com/s.php?q={}',
+    '!gh':      'https://github.com/search?o=desc&q={}&s=stars',
+    '!gist':    'https://gist.github.com/search?q={}',
+    '!gi':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
+    '!gn':      'https://news.google.com/search?q={}',
+    '!ig':      'https://www.instagram.com/explore/tags/{}',
+    '!m':       'https://www.google.com/maps/search/{}',
+    '!r':       'https://www.reddit.com/search?q={}',
+    '!sd':      'https://slickdeals.net/newsearch.php?q={}&searcharea=deals&searchin=first',
+    '!t':       'https://www.thesaurus.com/browse/{}',
+    '!tw':      'https://twitter.com/search?q={}',
+    '!w':       'https://en.wikipedia.org/wiki/{}',
+    '!yelp':    'https://www.yelp.com/search?find_desc={}',
+    '!yt':      'https://www.youtube.com/results?search_query={}'
+}
 
